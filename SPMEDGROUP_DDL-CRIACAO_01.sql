179ccb00ca7ca0c926dd3b201f4bbeffe057e724
Enzo Romano Panebianco <52742298835@info.local>
2019-02-11 14:30:24 -0200

    DDL & DML

---
+++ SPMEDGROUP_DDL-CRIACAO_01.sql
@@ -0,0 +1,54 @@
+create database SENAI_SPRINT1_PROJETO_FINAL
+
+create table Clinicas(
+	ID int identity primary key
+	,Nome_Fantasia varchar(255) not null unique
+	,Razao_Social varchar(255) not null unique
+	,CNPJ char(14) not null unique
+	,Endereço varchar(255)
+);
+
+create table Usuarios(
+	ID int identity primary key
+	,Nome varchar(255) not null
+	,Email varchar(255) not null
+	,Senha varchar(255) not null
+	,Tipo_Usuario bit not null
+	,ID_Clinica int foreign key references Clinicas(ID)
+);
+
+
+create table Especialidades(
+	ID int identity primary key
+	,Nome varchar(255) not null unique
+);
+
+create table Medicos (
+	ID int identity primary key 
+	,ID_Usuario int foreign key references Usuarios(ID) unique
+	,CRM int unique 
+	,ID_Especialidade int foreign key references Especialidades(ID)
+);
+
+create table Pacientes(
+	ID int identity primary key
+	,ID_Usuario int foreign key references Usuarios(ID) unique
+	,CPF char(11) not null unique
+	,RG char(11)  not null unique
+	,Endereço varchar(255) not null 
+	,DT_Nascimento date not null 
+);
+
+create table Situacao(
+	ID int identity primary key 
+	,Nome varchar(255) not null unique
+);
+
+create table Agendamentos(
+	ID int primary key
+	,ID_Paciente int foreign key references Pacientes(ID) not null
+	,ID_Medico int foreign key references Medicos(ID) not null
+	,DT_Agendamento date not null
+	,Descricao text not null
+	,ID_Situacao int foreign key references Situacao(ID) not null
+);
---
+++ SPMEDGROUP_DML_02.sql
@@ -0,0 +1,35 @@
+select * from Usuarios
+select Pacientes.ID_Usuario, Usuarios.Nome from Pacientes inner join Usuarios on Pacientes.ID_Usuario = Usuarios.ID
+		
+select * from Situacao
+select * from Usuarios
+select * from Medicos
+select * from Agendamentos
+select * from Clinicas
+
+
+insert into Pacientes(ID_Usuario,CPF,RG,Endereço,DT_Nascimento)
+Values  ('2','94839859000','43522543-5','Rua Estado de Israel 240, São Paulo, Estado de São Paulo, 04022-000','13/10/1983')
+		,('4','16839338002','54636525-3','Av. Ibirapuera - Indianópolis, 2927,  São Paulo - SP, 04029-200','10/10/1978')
+		,('6','91305348010','t32544444-1','R. Ver. Geraldo de Camargo, 66 - Santa Luzia, Ribeirão Pires - SP, 09405-380','27/08/1975')
+		,('7','79799299004','54566266-8','Alameda dos Arapanés, 945 - Indianópolis, São Paulo - SP, 04524-001','21/03/1972')
+		,('8','13771913039','54222126-8','R Sao Antonio, 232 - Vila Universal, Barueri - SP, 06407-140','05/03/2018')
+
+insert into Usuarios(Nome,Email,Senha,Tipo_Usuario,ID_Clinica)
+Values ('Alexandre','Ale@gmail.com','2121112',1,1)
+
+
+
+insert into Pacientes(ID_Usuario,CPF,RG,Endereço,DT_Nascimento)
+Values (11,'231141910','94322136-8','R. Vitória, 120 - Vila Sao Jorge, Barueri - SP, 06402-030','23/02/1995')
+
+insert into Agendamentos(ID_Paciente,ID_Medico,DT_Agendamento,Descricao,ID_Situacao)
+Values (11,3,'20/01/2019 15:00','OK',2)
+		,(17,1,'06/01/2018 10:00','OK',3)
+		,(12,1,'07/02/2019 11:00','Restam 10 dias de vida',2)
+		,(17,1,'06/02/2018  10:00:00','Instável',2)
+		,(16,2,'07/02/2019  11:00:45','OK',3)
+		,(11,3,'08/02/2019  15:00:00','OK',1)
+		,(16,2,'09/02/2019  11:00:45','Instável',1)
+
+select* from Pacientes
